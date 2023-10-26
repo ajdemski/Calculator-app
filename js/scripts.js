afc714b6cd676864a48da9fa7268aa8827a0d3ad
Aaron Demski <ajdemski@frontier.com>
2023-10-26 11:47:10 -0700

    add window alert for calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 // business logic
 function add(number1, number2) {
-  return number1 + number2;
+  return number1 + number2;4
 }
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return "the multiplied sum is " + number1 * number2;
+}
+
+function divide(number1, number2) {
+  return "the divided sum is " + number1 / number2;
+}
+
+function saySomething(whatToSay) {
+  window.alert(whatToSay)
+}
+
 
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+saySomething(divide(number1, number2));
